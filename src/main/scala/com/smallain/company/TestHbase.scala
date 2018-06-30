3be0aa925c9202d33e10b434bc0e7f8073f79696
smallain <smallainwuyuhang@163.com>
2018-07-01 00:38:51 +0800

    test

--- src/main/scala/com/smallain/company/TestHbase.scala
+++ src/main/scala/com/smallain/company/TestHbase.scala
@@ -7,17 +7,18 @@ import org.apache.hadoop.hbase.client.HTablePool
 object TestHbase {
   def main(args: Array[String]): Unit = {
     val myConf = HBaseConfiguration.create()
-    myConf.set("hbase.zookeeper.quorum", "iz2zea86z2leonw09hpjijz:2181,iz2zea86z2leonw09hpjimz:2181,iz2zea86z2leonw09hpjilz:2181,iz2zea86z2leonw09hpjikz:2181")
+    //myConf.set("hbase.zookeeper.quorum", "iz2zea86z2leonw09hpjijz:2181,iz2zea86z2leonw09hpjimz:2181,iz2zea86z2leonw09hpjilz:2181,iz2zea86z2leonw09hpjikz:2181")
+    myConf.set("hbase.zookeeper.quorum", "192.168.31.101:2181,192.168.31.102:2181,192.168.31.102:2181")
 
     val pool = new HTablePool(myConf, 2147483647)
 
 
     val dao = UsersDao(pool)
 
-    val user = dao.getUsers
-    user.foreach(x => println("用户是：" + x.user + " 姓名是：" + x.name + " 邮箱是：" + x.email + " 密码是：" + x.password))
+//    val user = dao.getUsers
+//    user.foreach(x => println("用户是：" + x.user + " 姓名是：" + x.name + " 邮箱是：" + x.email + " 密码是：" + x.password))
 
-    //val user = dao.addUser("xuxin","XuXin","xuxin@163.com","567789")
+    val user = dao.addUser("xuxin","XuXin","xuxin@163.com","567789")
 
     //user.foreach(x => println(x.name))
 
