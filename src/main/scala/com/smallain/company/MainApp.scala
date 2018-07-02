18efcfff9b9487a668d7e004088d15bcdd61ae41
Wu Yuhang <smallainwuyuhang@163.com>
2018-07-03 00:40:08 +0800

    20180703 add todo function

--- src/main/scala/com/smallain/company/MainApp.scala
+++ src/main/scala/com/smallain/company/MainApp.scala
@@ -35,6 +35,9 @@ object MainApp {
 			case x if x.contains("insert") => insertDataParse(x)
 		}
 		
+		//TODO 1.添加确定rowkey的唯一方法,不然每次都要添加手写的rowkey
+		//TODO 2.确认为什么有的时候连接kafka会很慢，达不到设定时间的同步，当然是基于原生的sparkstream，可能次情况与结构化流处理的不一致
+		//TODO 3.要点：因为spark是内存计算所以除了spark rdd外任何引用的外部对象都要序列化，通过添加with Serializable序列化属性可以轻松处理
 		ss1.foreachRDD { rdd =>
 			rdd.foreach { record =>
 				record.foreach(column=>td.putTable(column._1, "000004", "info", column._2, column._3))
