862fb70f1ca6cc8031b1312b7a0f7f0f29c5eb7e
smallain <smallainwuyuhang@163.com>
2018-07-16 18:41:50 +0800

    20180716 odsStream version-0.1

--- src/main/scala/com/smallain/company/Test.scala
+++ src/main/scala/com/smallain/company/Test.scala
@@ -1,10 +1,13 @@
 package com.smallain.company
-
+import com.smallain.utils.Md5Utils._
 import com.smallain.utils.TransLongTimeStamp._
 object Test extends App {
 	
 
 	val tm = "1530437707000"
+	val sss = md5HashString("wuyuhang666")
 	val a = transLongTimeStampToString(tm)
-	println(a)
+	println(sss)
 }
+
+
